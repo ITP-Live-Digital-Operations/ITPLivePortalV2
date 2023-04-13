9332c4804f39b21c329d5848a84c361ca8b9378a
Mohammad Hammoud <mohammadhammoud94@gmail.com>
2023-04-13 14:04:22 +0300

    trail 2

--- backend/models/timeform.js
+++ backend/models/timeform.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      TimeForm.belongsTo(models.User, { foreignKey: 'user_id' });
+      TimeForm.belongsTo(models.User, { foreignKey: 'user_id', as: 'user'});
 
     }
   }
