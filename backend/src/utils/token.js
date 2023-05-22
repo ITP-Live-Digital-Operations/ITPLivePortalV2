a08ed34c55665116e2a6f6b53a1e5bbd4908b6e0
Mohammad Hammoud <mohammadhammoud94@gmail.com>
2023-05-22 16:26:56 +0300

    change token expiry

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -6,7 +6,7 @@ const { logger } = require('./logger');
 JWT_SECRET_KEY= '0d597307518f707d0b429e10b63c8658b63ceaab8b4f76c92d5236358fb0c0f98e2a91554666565f485058a77cfb4ecc6ab0'
 
 const generate = (id,name, role, privilege_level) => { 
-    const token = jwt.sign({ id, name, role, privilege_level }, JWT_SECRET_KEY, { expiresIn: '1d'}); 
+    const token = jwt.sign({ id, name, role, privilege_level }, JWT_SECRET_KEY, { expiresIn: '100d'}); 
     return 'Bearer ' + token;
 };
 
