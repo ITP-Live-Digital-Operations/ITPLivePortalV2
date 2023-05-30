32550f537be87501a0a5905a4959779b4f8e08f4
Mohammad Hammoud <mohammadhammoud94@gmail.com>
2023-05-30 10:40:41 +0300

    token testing

--- frontend/src/app/core/Interceptors/token-interceptor.service.ts
+++ frontend/src/app/core/Interceptors/token-interceptor.service.ts
@@ -3,18 +3,20 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { NgZone } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private zone: NgZone) { }
 
-  token = localStorage.getItem('token');
+  token : any;
 
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.token = localStorage.getItem('token');
      let jwttoken = req.clone({
       setHeaders:{
         Authorization: ''+ this.token,
@@ -27,7 +29,7 @@ export class TokenInterceptorService implements HttpInterceptor {
   private handleError(err: HttpEvent<any>): Observable<any> {
     if (err instanceof HttpErrorResponse && err.status === 666) {
       // JWT expired, go to login
-      this.router.navigate(['/login']);
+      this.zone.run(() => this.router.navigate(['/login']));
   }
 
   // If it's not an authentication error, just throw it
