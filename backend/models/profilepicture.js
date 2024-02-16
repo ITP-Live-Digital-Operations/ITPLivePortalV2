8832122e9697b7da32db28fbee3baba1c4235644
Mohammad Hammoud <mohammadhammoud94@gmail.com>
2024-02-16 16:37:11 +0200

    Update model reference for profile picture to 'users' table

--- backend/models/profilepicture.js
+++ backend/models/profilepicture.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'User',
+        model: 'users',
         key: 'id'
       }
     },
