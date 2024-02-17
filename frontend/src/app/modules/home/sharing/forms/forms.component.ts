72a834e576f31849d7fcaa964414d6ecf27813a2
Charbel Azar <77696672+Charbel-Azar@users.noreply.github.com>
2024-02-17 23:15:58 +0200

    scroll wheel

--- frontend/src/app/modules/home/sharing/forms/forms.component.ts
+++ frontend/src/app/modules/home/sharing/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, ViewChild  } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/core/services/user.service';
 import { PATH } from 'src/app/core/constant/routes.constants';
@@ -9,6 +9,7 @@ import { PATH } from 'src/app/core/constant/routes.constants';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
+  @ViewChild('carousel', { static: true }) carousel!: ElementRef<HTMLDivElement>;
 
   public path = PATH;
   public privilegeLevel!: number;
@@ -24,7 +25,14 @@ export class FormsComponent {
   private getRole(): void { 
     this.userRole = this.userService.getRole();
   }
+  onWheel(event: WheelEvent): void {
+    event.preventDefault(); // Prevent the default wheel behavior (scrolling).
+    const container = this.carousel.nativeElement;
+    const toScroll = event.deltaY < 0 ? -80 : 80; // Adjust this value to control the scroll amount
 
+    // Smoothly scroll the container
+    container.scrollBy({ left: toScroll, behavior: 'auto' });
+  }
   private getPrivilegeLevel(): void {
     var token = localStorage.getItem('token');
     if (token && token.startsWith("Bearer ")) {
