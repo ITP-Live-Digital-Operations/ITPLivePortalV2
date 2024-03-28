5e1b294472e866a2432cf31ed7d7b66aac0a936a
lamaalsh <126325707+lamaalsh@users.noreply.github.com>
2024-03-29 00:47:14 +0200

    The forms of the new celebrity and new influencer have also been adjusted as those of the edited ones.

--- frontend/src/app/modules/home/emp/talent/create/new-influencer/new-influencer.component.ts
+++ frontend/src/app/modules/home/emp/talent/create/new-influencer/new-influencer.component.ts
@@ -139,7 +139,7 @@ export class NewInfluencerComponent {
     return valuesObject;
 }
 activeTabIndex: number = 0;
-tabCount: number = 3; 
+tabCount: number = 4; 
 nextTab() {
   if (this.activeTabIndex < this.tabCount - 1) {
     this.activeTabIndex++;
